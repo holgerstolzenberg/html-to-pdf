e6e87fe39dcc1bd07cf8e52d17f0139d26ecd0bc
Holger Stolzenberg <holger.stolzenberg@me.com>
2016-08-22 16:14:52 +0200

    Minor improvements to code

--- src/main/java/org/github/holgerstolzenberg/htmltopdf/ReportingService.java
+++ src/main/java/org/github/holgerstolzenberg/htmltopdf/ReportingService.java
@@ -3,17 +3,11 @@ package org.github.holgerstolzenberg.htmltopdf;
 import com.itextpdf.text.DocumentException;
 import org.slf4j.Logger;
 import org.w3c.dom.Document;
-import org.xhtmlrenderer.layout.SharedContext;
-import org.xhtmlrenderer.pdf.ITextOutputDevice;
 import org.xhtmlrenderer.pdf.ITextRenderer;
-import org.xhtmlrenderer.pdf.ITextUserAgent;
 import org.xhtmlrenderer.resource.XMLResource;
-import org.xhtmlrenderer.util.XRLog;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -41,9 +35,12 @@ final class ReportingService {
     try (final ByteArrayOutputStream os = new ByteArrayOutputStream()) {
       final Document doc = XMLResource.load(ReportingService.class.getResourceAsStream(uri)).getDocument();
 
-      ITextRenderer renderer = new ITextRenderer();
-      renderer.setDocument(doc, "classpath://report/");
-      renderer.getSharedContext().setUserAgentCallback(new UserAgentCallback(renderer.getOutputDevice(), renderer.getSharedContext()));
+      final ITextRenderer renderer = new ITextRenderer();
+      renderer.setDocument(doc, uri);
+
+      final ClasspathUserAgentCallback classpathUserAgentCallback = new ClasspathUserAgentCallback(renderer.getOutputDevice(), renderer.getSharedContext());
+      renderer.getSharedContext().setUserAgentCallback(classpathUserAgentCallback);
+
       renderer.layout();
       renderer.createPDF(os);
 
@@ -63,37 +60,4 @@ final class ReportingService {
     }
   }
 
-  private static class UserAgentCallback extends ITextUserAgent {
-    UserAgentCallback(ITextOutputDevice outputDevice, SharedContext sharedContext) {
-      super(outputDevice);
-      setSharedContext(sharedContext);
-    }
-
-    @Override
-    public String resolveURI(String uri) {
-      return uri;
-    }
-
-    @Override
-    protected InputStream resolveAndOpenStream(String uri) {
-      java.io.InputStream is = null;
-      System.out.println("------> " + uri);
-      URL url = UserAgentCallback.class.getResource("/report/" + uri);
-      System.out.println("-----> " + url);
-      if (url == null) {
-        XRLog.load("Didn't find resource [" + uri + "].");
-        return null;
-      }
-      try {
-        is = url.openStream();
-      } catch (java.net.MalformedURLException e) {
-        XRLog.exception("bad URL given: " + uri, e);
-      } catch (java.io.FileNotFoundException e) {
-        XRLog.exception("item at URI " + uri + " not found");
-      } catch (java.io.IOException e) {
-        XRLog.exception("IO problem for " + uri, e);
-      }
-      return is;
-    }
-  }
 }
